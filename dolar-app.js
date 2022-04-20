8cbbf6c96e3c9d3a0cef2098e13640c387b0aea7
Cristhian <galax13a@yahoo.es>
2022-04-20 13:24:56 -0500

    2 commit

--- dolar-app.js
+++ dolar-app.js
@@ -11,7 +11,8 @@ console.log(nDate);
     console.log("APP Node APi Dolar - rooms - crbs")
     const browser = await puper.launch({
         headless: true,
-        slowMo: 300
+        slowMo: 300,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     })
 
     console.log("Empezando  Dolar Web ... v1");
